eb29ce801058dfa39ddd8cf6c4bf3b186fda715e
aashviii <145525783+aashviii@users.noreply.github.com>
2024-12-04 11:41:58 +0530

    Add files via upload

---
+++ core python concept/creation of varibles and datatypes.py
@@ -0,0 +1,15 @@
+#integer
+age = 20
+print(age)
+
+#string
+name = "ashvi"
+print(name)
+
+#float
+weight = 50.44
+print(weight)
+
+#boolean
+status = True
+print(status)
---
+++ core python concept/user input.py
@@ -0,0 +1,8 @@
+name = input("enter the name")
+print(name)
+
+age= int(input("enter the age"))
+print(age)
+
+height = float(input("enter the height"))
+print(height)

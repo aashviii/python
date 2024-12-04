af582c6dbb300937b6bcb7259455129b68f5cef3
aashviii <145525783+aashviii@users.noreply.github.com>
2024-12-04 12:11:55 +0530

    Add files via upload

---
+++ control statement/break_statement.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "mango"]
+
+for fruit in fruits:
+    if fruit == "banana":
+        print("Banana found! Stop the loop.")
+        break
+    else:
+        print(f"Checking {fruit}..")

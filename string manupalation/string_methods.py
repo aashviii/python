bfdb8caf89834f87b946be9e2b52a00db09d6d0e
aashviii <145525783+aashviii@users.noreply.github.com>
2024-12-04 12:13:08 +0530

    Add files via upload

---
+++ string manupalation/string_methods.py
@@ -0,0 +1,7 @@
+name = input("enter the name:")
+
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.center(200))
+print(name.index("t"))
---
+++ string manupalation/string_slicing.py
@@ -0,0 +1,14 @@
+"""
+slicing
+0   1  2  3  4   5
+P   Y  T  H  O  N
+-6 -5 -4 -3 -2 -1
+"""
+#display first 3 letters from string
+
+name = "python".upper()
+print(name[0:3])
+print(name[2:5])
+print(name[2:6])
+print(name[2:])
+print(name[:4])
